7e02f133b129bfcb4ffe32142f1e8312792191b1
KanzaTahreem <tehreemmsk411@gmail.com>
2024-01-05 00:10:56 +0500

    fix(session): handle the session expired bug

--- utils/supabase-session.ts
+++ utils/supabase-session.ts
@@ -1,13 +1,8 @@
-import {  AuthResponse, SupabaseClient, createClient } from "@supabase/supabase-js";
-import { Database } from "@/types_db";
-import { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
+import {  SupabaseClient } from "@supabase/supabase-js";
 
-export const getSBSessionFromHeaders = async(headersList: ReadonlyHeaders, supabase: SupabaseClient): Promise<AuthResponse> => {
-  const access_token = headersList.get('authorization')?.split(' ')[1] || '';
-  const refresh_token = headersList.get('X-Refresh-Token') || headersList.get('x-refresh-token') || '';
-
-  const session = await supabase.auth.setSession({ access_token, refresh_token: ( typeof refresh_token === 'string' ) ? refresh_token : refresh_token[0] });
-  if (!session?.data?.user?.id) throw new Error('Session Expired');
+export const getSBSessionFromHeaders = async (supabase: SupabaseClient) => {
+  const session = await supabase.auth.getSession();
+  if (session?.error) throw new Error('Session Expired');
   return session;
 };
 
