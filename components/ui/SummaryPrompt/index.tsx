1f3a4ab492f11e99598d306526ab299975e7148c
KanzaTahreem <tehreemmsk411@gmail.com>
2024-01-04 15:50:46 +0500

    feat(summary-prompt): create a summary prompt component

---
+++ components/ui/SummaryPrompt/index.tsx
@@ -0,0 +1,77 @@
+'use client';
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment, useEffect } from 'react';
+import copyText from '@/public/assets/icons/copy.svg';
+import Image from 'next/image';
+import Button from '@/components/ui/Button';
+
+interface Props {
+  title: string;
+  isOpen: boolean;
+  setIsOpen: Function;
+}
+
+const SummaryPrompt: React.FC<Props> = ({ title, isOpen, setIsOpen }) => {
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    console.log('TITLE', title, isOpen)
+  }, [title])
+
+  return (
+    <>
+      {isOpen && (
+        <Transition appear show={isOpen} as={Fragment}>
+          <Dialog as="div" className="fixed inset-0 z-50 overflow-y-auto" onClose={toggleSidebar}>
+            <div className="flex items-center justify-center min-h-screen">
+              <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Dialog.Overlay className="fixed inset-0 bg-black/50" />
+                </Transition.Child>
+                <Transition.Child
+                  as={Fragment}
+                  enter="transition-transform ease-out duration-300"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transition-transform ease-in duration-300"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                <div className="fixed h-full right-0 w-96 bg-[#15192D] shadow-md flex flex-col justify-between">
+                  <div className="flex flex-col gap-6 py-7 px-6">
+                    <div className='flex justify-between items-center'>
+                      <Dialog.Title as="h3" className="text-lg text-[#ECEDEE] font-semibold tracking-[0.05rem]">
+                        {title}
+                      </Dialog.Title>
+                      <div className='flex gap-2 items-center'>
+                        <div className='bg-[#1C274F] rounded-full w-8 h-8 flex items-center justify-center'>
+                          <Image alt="copy icon" src={copyText} />
+                        </div>
+                        <Button variant='primary' shape='solid' height={36} fontSize={14}>Download</Button>
+                      </div>
+                    </div>
+                    <Dialog.Description as='p' className="text-sm text-[#ECEDEE] text-sm tracking-[0.04038rem]">
+                      Summarize the entire transcript or choose a custom duration.
+                    </Dialog.Description>
+                  </div>
+                  <p className='bg-[#11131F] py-3.5 px-6 text-center text-[#ECEDEE] text-[0.9375rem] cursor-pointer' onClick={toggleSidebar}>Cancel and go back</p>
+                </div>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition>
+      )}
+    </>
+  );
+};
+
+export default SummaryPrompt;
